39c9e2a5a0c0746061dade23954de11d9e6cdec0
Liam Henke <henld003@mymail.unisa.edu.au>
2024-05-22 15:17:12 +0930

    Moved into one folder and added player file

--- all_that_dice/player.py
+++ all_that_dice/player.py
@@ -1,3 +1,13 @@
+#
+# File: player.py
+# Description: Player file for dice game.
+# Author: Liam Henke
+# Student ID: 110377752
+# Email ID: henld003
+# This is my own work as defined by 
+#    the University's Academic Misconduct Policy.
+#
+
 class Player:
     def __init__(self, name):
         self._name = name
