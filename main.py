050009157db0d0ff3afceef2ba37ee9a2601f870
Liam Henke <henld003@mymail.unisa.edu.au>
2024-05-22 15:05:36 +0930

    Added main file

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+# File: main.py
+# Description: Main file for dice game.
+# Author: Liam Henke
+# Student ID: 110377752
+# Email ID: henld003
+# This is my own work as defined by 
+#    the University's Academic Misconduct Policy.
+#
+
+from all_that_dice import AllThatDice
+
+if __name__ == "__main__":
+    game = AllThatDice()
+    game.run()
